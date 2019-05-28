03992d52aed1e970f2d877215d9154886d0758c9
gagelarsen <glarsen@aquaveo.com>
2019-05-28 14:29:47 -0600

    use python36

--- build.py
+++ build.py
@@ -21,7 +21,6 @@ if __name__ == "__main__":
         # General Options
         env_vars.update({
             'XMS_VERSION': XMS_VERSION,
-            'VERBOSE': 1,
             'PYTHON_TARGET_VERSION': python_target_version,
             'RELEASE_PYTHON': release_python,
             'AQUAPI_USERNAME': aquapi_username,
