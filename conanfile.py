756755d80ff51961a5393e61610d057d38ebe254
Gage Larsen <glarsen@aquaveo.com>
2025-01-22 16:10:31 -0700

    update to conan2

--- conanfile.py
+++ conanfile.py
@@ -1,24 +1,21 @@
 """
 conanfile.py for the xmsmesher project.
 """
-from xmsconan.xms_conan_file import XmsConanFile
+from xmsconan.xms_conan2_file import XmsConan2File
 
 
-class XmsmesherConan(XmsConanFile):
+class XmsmesherConanFile(XmsConan2File):
     """
-    XmsstamperConan class used for defining the conan info.
+    XmsmesherConanFile class used for defining the conan info.
     """
-    name = 'xmsmesher'
-    url = 'https://github.com/Aquaveo/xmsmesher'
-    description = 'Mesher library for XMS products'
+    name = "xmsmesher"
+    url = "https://github.com/Aquaveo/xmsmesher"
+    description = "Meshing library for XMS products"
     xms_dependencies = [
-        'xmscore/[>=6.0.1 <7.0.0]@aquaveo/stable',
-        'xmsgrid/[>=7.8.4 <8.0.0]@aquaveo/stable',
-        'xmsinterp/[>=6.0.1 <7.0.0]@aquaveo/stable',
+        "xmscore/6.2.5-6-g72e83110",
+        "xmsgrid/5.5.3-53-g35e7e2f",
+        "xmsinterp/6.1.1-12-g1d45191a",
     ]
-    extra_export_sources = [
-        'test_files'
-    ]
+    extra_export_sources = ['test_files']
 
-
-LIBRARY_NAME = XmsmesherConan.name
+LIBRARY_NAME = XmsmesherConanFile.name
